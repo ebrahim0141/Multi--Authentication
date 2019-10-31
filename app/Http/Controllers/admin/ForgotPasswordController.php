ccdcbaceb7c00d4231b9d03fe564db0584fd87b2
ebrahim0141 <eme.pust@gmail.com>
2019-11-01 00:18:56 +0600

    added multiauth file

---
+++ app/Http/Controllers/admin/ForgotPasswordController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+
+class ForgotPasswordController extends Controller
+{
+    use SendsPasswordResetEmails;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest:admin');
+    }
+
+    public function showLinkRequestForm()
+    {
+        return view('admin.passwords.email');
+    }
+    public function broker()
+    {
+        return Password::broker('admins');
+    }
+}
